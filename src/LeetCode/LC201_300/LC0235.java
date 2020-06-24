8264491d13ccdad0ee9efb116e29271b9e6ad6c7
Bohan Zhang <zhangbh@uw.edu>
2020-06-24 15:23:33 -0700

    235

---
+++ src/LeetCode/LC201_300/LC0235.java
@@ -0,0 +1,60 @@
+package LeetCode.LC201_300;
+
+import LeetCode.TreeNode;
+
+/**
+ * Lowest Common Ancestor of a Binary Search Tree
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia:
+ * “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that
+ * has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ * Example 1:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * Example 2:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ *
+ * Note:
+ *
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ */
+public class LC0235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root; // root.val is between p.val and q.val, then it is the LCA
+    }
+
+    public TreeNode iteration(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (p.val < cur.val && q.val < cur.val) {
+                cur = cur.left;
+            } else if (p.val > cur.val && q.val > cur.val) {
+                cur = cur.right;
+            } else {
+                return cur;
+            }
+        }
+        return null;
+    }
+}
