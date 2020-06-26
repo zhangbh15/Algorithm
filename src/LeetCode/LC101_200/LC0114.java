48aaa7822ff851806865d09d340fd9c62fd16278
Bohan Zhang <zhangbh@uw.edu>
2020-06-25 20:46:29 -0700

    114

--- src/LeetCode/LC101_200/LC0114.java
+++ src/LeetCode/LC101_200/LC0114.java
@@ -30,6 +30,57 @@ import LeetCode.TreeNode;
  */
 public class LC0114 {
     public void flatten(TreeNode root) {
+        if (root != null) {
+            helper(root);
+        }
+    }
 
+    private TreeNode helper(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        if (root.left == null) {
+            return helper(root.right);
+        }
+        if (root.right == null) {
+            root.right = root.left;
+            root.left = null;
+            return helper(root.right);
+        }
+
+        TreeNode leftTail = helper(root.left);
+        TreeNode rightTail = helper(root.right);
+
+        leftTail.right = root.right;
+        leftTail.left = null;
+
+        root.right = root.left;
+        root.left = null;
+
+        return rightTail;
+    }
+
+    public void flattenIter(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                cur = cur.right;
+                continue;
+            }
+
+            TreeNode leftTail = cur.left;
+            while (leftTail.right != null) {
+                leftTail = leftTail.right;
+            }
+
+            leftTail.right = cur.right;
+            cur.right = cur.left;
+            cur.left = null;
+            cur = cur.right;
+        }
     }
 }
