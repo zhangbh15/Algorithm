5fb25e907a7b7ec5803d99d9fca8d8460532682d
Bohan Zhang <zhangbh@uw.edu>
2020-07-12 12:10:32 -0700

    74

--- src/LeetCode/LC1_100/LC0074.java
+++ src/LeetCode/LC1_100/LC0074.java
@@ -31,24 +31,31 @@ package LeetCode.LC1_100;
  */
 public class LC0074 {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0
-                || matrix[0] == null || matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0 ||
+                matrix[0] == null || matrix[0].length == 0) {
             return false;
         }
 
-        int i = matrix.length - 1;
-        int j = 0;
-        while (i >= 0 && j < matrix[0].length) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int start = 0, end = rows * cols - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int i = mid / cols;
+            int j = mid % cols;
+
             if (matrix[i][j] == target) {
                 return true;
             }
 
-            if (matrix[i][j] > target) {
-                i--;
-            } else if (matrix[i][j] < target) {
-                j++;
+            if (matrix[i][j] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
+
         return false;
     }
 }
