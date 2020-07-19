f00d4803bfd18b9944faf7b693db1cd29105f231
Bohan Zhang <zhangbh@uw.edu>
2020-07-19 11:59:15 -0700

    20

---
+++ src/LeetCode/LC1_100/LC0020.java
@@ -0,0 +1,70 @@
+package LeetCode.LC1_100;
+
+import java.util.Stack;
+
+/**
+ * Validate Parentheses
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ *
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ *
+ * Input: "{[]}"
+ * Output: true
+ */
+public class LC0020 {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (ch == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else {
+                throw new IllegalArgumentException("Unsupported character");
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
