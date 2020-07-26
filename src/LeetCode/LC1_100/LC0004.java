2c508cdf80df76c82a75cdc7f3477a52ce107e58
Bohan Zhang <zhangbh@uw.edu>
2020-07-25 18:53:49 -0700

    4

--- src/LeetCode/LC1_100/LC0004.java
+++ src/LeetCode/LC1_100/LC0004.java
@@ -24,6 +24,44 @@ package LeetCode.LC1_100;
  */
 public class LC0004 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return  0.0;
+        }
 
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int mid = (len1 + len2 + 1) / 2; // TODO: long
+
+        int start = 0;
+        int end = Math.min(len1, mid);
+        int i = 0;
+        int j = 0;
+        while (start <= end) {
+            i = start + (end - start) / 2; // number of elements from nums1
+            j = mid - i; // number of elements from nums2
+
+            if (i < end && nums1[i] < nums2[j - 1]) {
+                start = i + 1;
+            } else if ( i > start && nums2[j] < nums1[i - 1]) { //
+                end = i - 1;
+            } else {
+                break;
+            }
+        }
+
+        double median = i == 0 ? nums2[j - 1] :
+                (j == 0 ? nums1[i - 1] : Math.max(nums1[i - 1], nums2[j - 1]));
+        if ((len1 + len2) % 2 == 0) {
+            median += i == len1 ? nums2[j] :
+                    (j == len2 ? nums1[i] : Math.min(nums1[i], nums2[j]));
+            median /= 2;
+        }
+        return median;
     }
 }
