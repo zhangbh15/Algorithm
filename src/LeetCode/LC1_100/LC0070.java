df356c92291f0cac577009ef732080efa55a04f5
Bohan Zhang <zhangbh@uw.edu>
2020-07-31 20:35:48 -0700

    70

---
+++ src/LeetCode/LC1_100/LC0070.java
@@ -0,0 +1,47 @@
+package LeetCode.LC1_100;
+
+/**
+ * Climbing Stairs
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 45
+ */
+public class LC0070 {
+    public int climbStairs(int n) {
+        if (n <= 3) {
+            return n;
+        }
+
+        int prev = 3;
+        int prevPrev = 2;
+        for (int i = 4; i <= n; i++) {
+            int cur = prev + prevPrev;
+            prevPrev = prev;
+            prev = cur;
+        }
+
+        return prev;
+    }
+}
