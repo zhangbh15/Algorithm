8a378be3f461defe9e2b037d0c1836ce95d3f9c9
Bohan Zhang <zhangbh@uw.edu>
2020-08-02 17:27:54 -0700

    45

--- src/LeetCode/LC1_100/LC0045.java
+++ src/LeetCode/LC1_100/LC0045.java
@@ -20,4 +20,33 @@ package LeetCode.LC1_100;
  * You can assume that you can always reach the last index.
  */
 public class LC0045 {
+    public int jump(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        int len= nums.length;
+        int[] dp = new int[len];
+        for (int i = 0; i < len; i++) {
+            // if (dp[i] == 0) {return -1;}
+
+            int maxJump = Math.min(len - 1, i + nums[i]);
+            while (maxJump >= 0 && dp[maxJump] == 0) {
+                dp[maxJump--] = dp[i] + 1;
+            }
+
+            if (dp[len - 1] != 0) {
+                return dp[len - 1];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        LC0045 so = new LC0045();
+        int[] nums = {2, 3, 1, 1, 4};
+        int res = so.jump(nums);
+        System.out.println(res);
+    }
 }
