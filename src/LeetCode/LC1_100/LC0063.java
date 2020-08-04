cdc1832308b540b334331aea8446e7c7ed06b0d6
Bohan Zhang <zhangbh@uw.edu>
2020-08-03 21:03:36 -0700

    63

---
+++ src/LeetCode/LC1_100/LC0063.java
@@ -0,0 +1,82 @@
+package LeetCode.LC1_100;
+
+/**
+ * Unique Paths II
+ *
+ * A robot is located at the top-left corner of a m x n grid
+ * (marked 'Start' in the diagram below).
+ *
+ * The robot can only move either down or right at any point
+ * in time. The robot is trying to reach the bottom-right corner
+ * of the grid (marked 'Finish' in the diagram below).
+ *
+ * Now consider if some obstacles are added to the grids. How
+ * many unique paths would there be?
+ *
+ *
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ *
+ * Note: m and n will be at most 100.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   [0,0,0],
+ *   [0,1,0],
+ *   [0,0,0]
+ * ]
+ * Output: 2
+ * Explanation:
+ * There is one obstacle in the middle of the 3x3 grid above.
+ * There are two ways to reach the bottom-right corner:
+ * 1. Right -> Right -> Down -> Down
+ * 2. Down -> Down -> Right -> Right
+ */
+public class LC0063 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 ||
+                obstacleGrid[0] == null || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                obstacleGrid[i][0] = 1;
+            } else {
+                for (int j = i; j < rows; j++) {
+                    obstacleGrid[j][0] = 0;
+                }
+                break;
+            }
+        }
+
+        for (int i = 1; i < cols; i++) {
+            if (obstacleGrid[0][i] == 0) {
+                obstacleGrid[0][i] = 1;
+            } else {
+                for (int j = i; j < cols; j++) {
+                    obstacleGrid[0][j] = 0;
+                }
+                break;
+            }
+        }
+
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }
+
+                obstacleGrid[i][j] = obstacleGrid[i- 1][j] + obstacleGrid[i][j - 1];
+            }
+        }
+
+        return obstacleGrid[rows - 1][cols - 1];
+    }
+}
