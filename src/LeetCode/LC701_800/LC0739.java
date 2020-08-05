832aafb90e08d71816ebc50b9783c7fca3e64f27
Bohan Zhang <zhangbh@uw.edu>
2020-08-04 20:52:49 -0700

    739

---
+++ src/LeetCode/LC701_800/LC0739.java
@@ -0,0 +1,40 @@
+package LeetCode.LC701_800;
+
+import java.util.Stack;
+
+/**
+ * Daily Temperature
+ *
+ * Given a list of daily temperatures T, return a list such that, for each day
+ * in the input, tells you how many days you would have to wait until a warmer
+ * temperature. If there is no future day for which this is possible, put 0 instead.
+ *
+ * For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73],
+ * your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+ *
+ * Note: The length of temperatures will be in the range [1, 30000]. Each temperature
+ * will be an integer in the range [30, 100].
+ */
+public class LC0739 {
+    public int[] dailyTemperatures(int[] T) {
+        if (T == null || T.length == 0) {
+            return T;
+        }
+
+        int len = T.length;
+        int[] res = new int[len];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int popDay = stack.pop();
+                res[popDay] = i - popDay;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
