5a9826b8584989888ee344fcfb8e95a889757b49
Bohan Zhang <zhangbh@uw.edu>
2020-08-11 20:52:37 -0700

    231

---
+++ src/LeetCode/LC201_300/LC0231.java
@@ -0,0 +1,36 @@
+package LeetCode.LC201_300;
+
+/**
+ * Power of Two
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+ *
+ * Example 1:
+ *
+ * Input: 1
+ * Output: true
+ * Explanation: 20 = 1
+ * Example 2:
+ *
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * Example 3:
+ *
+ * Input: 218
+ * Output: false
+ */
+public class LC0231 {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        int numOnes = 0;
+        for (int i = 0; i < 31; i++) {
+            numOnes += 1 & (n >> i);
+        }
+
+        return numOnes == 1;
+    }
+}
