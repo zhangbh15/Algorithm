835fe1575ca44730c10396ebbf392afb2a13411e
Bohan Zhang <zhangbh@uw.edu>
2020-08-16 14:44:21 -0700

    328

---
+++ src/LeetCode/LC301_400/LC0328.java
@@ -0,0 +1,49 @@
+package LeetCode.LC301_400;
+
+import LeetCode.util.ListNode;
+
+/**
+ * Odd Even Linked List
+ *
+ * Given a singly linked list, group all odd nodes together followed by the even nodes.
+ * Please note here we are talking about the node number and not the value in the nodes.
+ *
+ * You should try to do it in place. The program should run in O(1) space complexity and
+ * O(nodes) time complexity.
+ *
+ * Example 1:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 1->3->5->2->4->NULL
+ * Example 2:
+ *
+ * Input: 2->1->3->5->6->4->7->NULL
+ * Output: 2->3->6->7->1->5->4->NULL
+ *
+ *
+ * Constraints:
+ *
+ * The relative order inside both the even and odd groups should remain as it was in the input.
+ * The first node is considered odd, the second node even and so on ...
+ * The length of the linked list is between [0, 10^4].
+ */
+public class LC0328 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
