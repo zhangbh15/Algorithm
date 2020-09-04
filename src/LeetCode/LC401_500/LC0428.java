2facfd93cf7bc843302a09e58a7ca55ad4e087ed
Bohan Zhang <zhangbh@uw.edu>
2020-09-04 12:13:47 -0700

    432

--- src/LeetCode/LC401_500/LC0428.java
+++ src/LeetCode/LC401_500/LC0428.java
@@ -57,24 +57,26 @@ import java.util.*;
 // codec.deserialize(codec.serialize(root));
 
 
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
 
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
 
 public class LC0428 {
+    // Definition for a Node.
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
     // Encodes a tree to a single string.
     public String serialize(Node root) {
         return null;
