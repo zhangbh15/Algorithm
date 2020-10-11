51cb360d263c1dbbdb3d7551de3e4a09b47937cd
Bohan Zhang <zhangbh@uw.edu>
2020-10-10 17:13:10 -0700

    312

--- src/LeetCode/LC301_400/LC0312.java
+++ src/LeetCode/LC301_400/LC0312.java
@@ -23,4 +23,34 @@ package LeetCode.LC301_400;
  *              coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
  */
 public class LC0312 {
+    public int maxCoins(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException();
+        }
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int len = nums.length;
+        int[][] dp = new int[len][len];
+
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i; j < len; j++) {
+                int leftBound = i == 0 ? 1 : nums[i - 1];
+                int rightBound = j == len - 1 ? 1 : nums[j + 1];
+
+                for (int k = i; k <= j; k++) {
+                    int leftSub = k == i ? 0 : dp[i][k - 1];
+                    int rightSub = k == j ? 0 : dp[k + 1][j];
+                    int coin = leftSub + rightSub + nums[k] * leftBound * rightBound;
+                    // Always burst the balloon at k last, so that the boundaries of
+                    // the left and right subranges are fixed.
+
+                    dp[i][j] = Math.max(dp[i][j], coin);
+                }
+            }
+        }
+
+        return dp[0][len - 1];
+    }
 }
